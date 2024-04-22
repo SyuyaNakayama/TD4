d8ea9b5273fea049b4c8b5c47b53e09dca91b1b1
NoseAlien <k021g1117@g.neec.ac.jp>
2024-04-22 15:03:26 +0900

    プレイヤーの動き途中まで

--- Assets/Scripts/LiveEntity.cs
+++ Assets/Scripts/LiveEntity.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LiveEntity : MonoBehaviour
 {
+    protected Vector3 movement;
+
     void Start()
     {
         
@@ -11,6 +13,21 @@ public class LiveEntity : MonoBehaviour
 
     void FixedUpdate()
     {
+        LiveEntityUpdate();
+
+        GetComponent<Rigidbody>().velocity = transform.rotation * movement;
+
+        movement *= 0.8f;
+        movement += new Vector3(0, -0.5f, 0);
+    }
+
+    void OnCollisionStay(Collision col)
+    {
         
+    }
+
+    protected virtual void LiveEntityUpdate()
+    {
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class Player : LiveEntity
 {
-    
+    protected override void LiveEntityUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movement += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movement += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            movement = new Vector3(movement.x, 10, movement.z);
+        }
+    }
 }
