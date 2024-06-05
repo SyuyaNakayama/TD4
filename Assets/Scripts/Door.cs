343750fcca6887347f840079ec6a0b2aef5240b3
KX_net <kx.net.com@gmail.com>
2024-06-05 16:26:21 +0900

    ステージの入り口とゴールの見た目変更

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,6 +6,16 @@ public class Door : MonoBehaviour
 {
     [SerializeField]
     string nextScene;
+
+    void OnWillRenderObject()
+    {
+        Vector2 tiling = new Vector2(8 * Screen.width / (float)Screen.height, 8);
+        Vector2 offset = new Vector2(Mathf.Repeat(Time.time / 8, 1), Mathf.Repeat(Time.time / 8, 1));
+        GetComponent<Renderer>().materials[0].SetTextureScale("_MainTex", tiling);
+        GetComponent<Renderer>().materials[0].SetTextureOffset("_MainTex", offset);
+
+        GetComponent<Renderer>().materials[0].SetFloat("_MainAlpha", 0.5f);
+    }
     void OnTriggerStay(Collider col)
     {
         if (col.gameObject.GetComponent<Player>() != null)
