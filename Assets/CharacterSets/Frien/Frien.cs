64274292624fc18dc1c7aaa3a48c0e315711b731
KX_net <kx.net.com@gmail.com>
2024-06-17 15:29:37 +0900

    フレンの動き　ひとまず完成

--- Assets/CharacterSets/Frien/Frien.cs
+++ Assets/CharacterSets/Frien/Frien.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 //フレン
 public class Frien : Enemy
 {
-    const int maxAwakeTimeframe = 600;
+    const float moveSpeed = 1;
 
-    int awakeTimeFrame;
+    bool awake;
 
     protected override void LiveEntityUpdate()
     {
-        if(awakeTimeFrame > 0)
+        //まず周囲にいる見方をサーチ
+        LiveEntity[] friends = GetFriends();
+        //サーチした味方の内一体でも死んでいたら目を覚ます
+        for (int i = 0; i < friends.Length; i++)
         {
+            if (!friends[i].IsLive())
+            {
+                awake = true;
+                break;
+            }
+        }
+
+        //目を覚ましている時
+        if(awake)
+        {
+            //近くに敵がいたら接近
+            if(GetNearestTarget() != null)
+            {
+                targetCursor = transform.InverseTransformPoint(
+                GetNearestTarget().transform.position).normalized;
+                Move(GetMovement() + targetCursor * moveSpeed);
+            }
+            //常に攻撃
             if (!IsAttacking())
             {
-                SetAttackMotion("Bite");
+                SetAttackMotion(GetData().GetDefaultAttackMotionName());
             }
         }
     }
