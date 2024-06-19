0473b65afa86e0731606da9108a76761cb405d06
KX_net <kx.net.com@gmail.com>
2024-06-19 13:01:44 +0900

    とりあえずキモイムを追加　詳細な動きは後で

---
+++ Assets/CharacterSets/Kimoime/Kimoime.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ÉLÉÇÉCÉÄ
+public class Kimoime : Enemy
+{
+    protected override void LiveEntityUpdate()
+    {
+        
+    }
+}
