cecba89848502f579df2d41bfc8b2aa4b80c1a8d
KX_net <kx.net.com@gmail.com>
2024-07-08 16:07:15 +0900

    新しい敵完成　早速テストシーンに行って正体を確かめよう！

---
+++ Assets/CharacterSets/Uzaime/Uzaime.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ÉEÉUÉCÉÄ
+public class Uzaime : Enemy
+{
+    const float stickRadius = 0.5f;
+    const float stickPower = 10;
+
+    protected override void LiveEntityUpdate()
+    {
+        LiveEntity[] targets = GetTargets();
+        for (int i = 0; i < targets.Length; i++)
+        {
+            LiveEntity target = targets[i];
+            if (Vector3.Magnitude(transform.InverseTransformPoint(
+                target.transform.position)) <= stickRadius)
+            {
+                target.Move(
+                    target.transform.InverseTransformPoint(transform.position)
+                    * stickPower);
+            }
+            //çUåÇìÆçÏíÜÇ≈Ç»Ç¢éûÇ…älï®Çå©Ç¬ÇØÇΩÇÁçUåÇìÆçÏÇ÷
+            else if (!IsAttacking() && GetNearestTarget() != null)
+            {
+                //ë_Ç§
+                targetCursor = transform.InverseTransformPoint(
+                    GetNearestTarget().transform.position);
+                transform.Rotate(0,
+                    Mathf.Atan2(targetCursor.x, targetCursor.z) / Mathf.Deg2Rad,
+                    0, Space.Self);
+                //çUåÇÉÇÅ[ÉVÉáÉìÇçƒê∂
+                SetAttackMotion(GetData().GetDefaultAttackMotionName());
+            }
+        }
+    }
+}
