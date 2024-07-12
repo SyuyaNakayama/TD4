d665de10578e4b57e794fbd0587d0d8aef4f90d0
KX_net <kx.net.com@gmail.com>
2024-07-12 14:17:04 +0900

    テストステージにキャラ切り替えアイテムが出現！南側の浮島に行ってみよう！まだ拾っても効果はないけど…

--- Assets/Scripts/GeoGroObject.cs
+++ Assets/Scripts/GeoGroObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 [DisallowMultipleComponent]
 public class GeoGroObject : UnLandableObject
 {
