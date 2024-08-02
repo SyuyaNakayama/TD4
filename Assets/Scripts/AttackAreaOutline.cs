5da061fcabe8ecdb5bc605b2547677519490a1d8
KX_net <kx.net.com@gmail.com>
2024-08-03 08:34:04 +0900

    攻撃判定表示の色がキャラと同じになるようにした

---
+++ Assets/Scripts/AttackAreaOutline.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAreaOutline : MonoBehaviour
+{
+    [SerializeField]
+    AttackArea attackArea;
+    [SerializeField]
+    MeshRenderer meshRenderer;
+    [SerializeField]
+    int materialIndex;
+
+    void OnWillRenderObject()
+    {
+        if (attackArea && attackArea.GetAttacker()
+        && meshRenderer && meshRenderer.materials.Length > materialIndex)
+        {
+            Color outlineColor =
+                attackArea.GetAttacker().GetData().GetThemeColor();
+            outlineColor.a = 1;
+            meshRenderer.materials[materialIndex].SetColor(
+                "_OutlineColor", outlineColor);
+        }
+    }
+}
